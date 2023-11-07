1b52f2ee3f153c2892494e7ab918377c40ecfea2
Arthur Cathelain <114919245+arkaht@users.noreply.github.com>
2023-11-07 12:32:54 +0100

    [fixed] BaseCharacter implementable event functions not overridable

--- Source/MoBArtFX/Public/BaseCharacter.h
+++ Source/MoBArtFX/Public/BaseCharacter.h
@@ -26,9 +26,8 @@ public:
 	// Sets default values for this character's properties
 	ABaseCharacter();
 
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable )
 	void Death();
-	virtual void Death_Implementation() {};
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -37,23 +36,18 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	// Attacks
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable )
 	void AutoAttack();
-	virtual void AutoAttack_Implementation();
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable )
 	void Spell_01();
-	virtual void Spell_01_Implementation() {}
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable )
 	void Spell_02();
-	virtual void Spell_02_Implementation() {}
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable )
 	void Ultimate();
-	virtual void Ultimate_Implementation() {}
 
-	
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable )
 	void Reload();
-	virtual void Reload_Implementation();
 
 	// PlayerDatas
 	UFUNCTION(BlueprintCallable, BlueprintPure)
