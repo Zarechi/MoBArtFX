9db29860700ead99d5272161bbe15aa85bf62d54
Cyril Crozat <ccrozat@artfx.fr>
2023-11-16 17:55:04 +0100

    [added] damage on cainer bullet

--- Source/MoBArtFX/Public/Characters/Cainer/CainerInfos.h
+++ Source/MoBArtFX/Public/Characters/Cainer/CainerInfos.h
@@ -40,4 +40,7 @@ public:
 	float flashRange = 3000.0f;
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Spells|Ultimate", meta = (min = 0.0f))
 	float flashDuration = 1.0f;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Spells|AutoAttack", meta = (min = 0.0f))
+	float autoattack_damage = 1.0f;
 };
